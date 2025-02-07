7bbf5c798d852800ef7e1fa4890fa3206f5692ce
Amaya Wickramaarachchi <apiyumi7@email.com>
2025-02-07 11:29:30 +0530

    CI CD TEST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li><h1>TEST CI CD</h1></li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
